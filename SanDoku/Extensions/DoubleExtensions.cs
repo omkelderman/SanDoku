0da04c77e02fd80f5bf464b120f620ca7f542c27
Olle Kelderman <omkelderman@gmail.com>
2023-04-21 15:22:17 +0200

    update to net7 and simplify project structure

--- SanDoku/Extensions/DoubleExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SanDoku.Extensions;
-
-public static class DoubleExtensions
-{
-    public static double? NaNOrInfinityToNull(this double d)
-    {
-        return double.IsNaN(d) || double.IsInfinity(d) ? null : d;
-    }
-}
---
+++ SanDoku/Util/CustomRulesetStore.cs
@@ -0,0 +1,13 @@
+﻿using osu.Game.Rulesets;
+
+namespace SanDoku.Util;
+
+public class CustomRulesetStore : RulesetStore
+{
+    public CustomRulesetStore(IEnumerable<RulesetInfo> availableRulesets)
+    {
+        AvailableRulesets = availableRulesets;
+    }
+
+    public override IEnumerable<RulesetInfo> AvailableRulesets { get; }
+}
